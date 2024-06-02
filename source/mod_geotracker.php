5d106f99b5897836f351bcf82f188c4963bd4732
Phil Petree <phil.petree@gmail.com>
2024-06-02 14:33:33 -0400

    Update mod_geotracker.php to version 1.9.0

--- source/mod_geotracker.php
+++ source/mod_geotracker.php
@@ -1,19 +1,18 @@
 <?php
 /**
 
- * GeoTracker Module - Shows your sites visitors on a Google Map
- *
+ * GeoTracker Module 
+
  * @link http://github.com/ppetree/geotracker
- * @copyright Copyright (C) 2023 Phil Petree
+ * @copyright Copyright (C) 2023-2024 Phil Petree
  * @license https://github.com/ppetree/geotracker/blob/main/LICENSE GNU/GPL 
- * @version 1.8.85
- *
+ * @version 1.9.0
+ * Shows your visitors on a Google Map
  */
 
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
  
-
 // Include the syndicate functions only once
 require_once( dirname(__FILE__).'/helper.php' );
 require( JModuleHelper::getLayoutPath( 'mod_geotracker' ) );
